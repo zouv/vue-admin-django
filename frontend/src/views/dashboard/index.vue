7c81f44b1b53bd619a2ec078d4aa991d87594d59
laoseng <1572665580@qq.com>
2019-10-14 01:44:33 -0700

    精简无用页面

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -31,12 +31,9 @@
 
 <script>
   import {mapGetters} from 'vuex'
-  import adminDashboard from './admin'
-  import editorDashboard from './editor'
 
   export default {
     name: 'Dashboard',
-    components: {adminDashboard, editorDashboard},
     data() {
       return {
         currentRole: 'adminDashboard',
